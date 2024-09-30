2abc556c03aed26a0e3b16f6a9219b3fdfbf710d
Viktor Grabov <viktor@grabov.ru>
2024-10-01 02:53:56 +0300

    feat: autocompletion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,32 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			-- TODO: python, js, ts, html, css
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            -- TODO: python, js, ts, html, css
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
