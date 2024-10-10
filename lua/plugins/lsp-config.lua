cdbd891609f216345ca1a34e3674a26f64d33f58
Viktor Grabov <viktor@grabov.ru>
2024-10-10 12:18:53 +0300

    feat: add new LSPs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,48 +1,51 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            -- TODO: python, js, ts, html, css
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright", "ts_ls", "html", "cssls" },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright", "ts_ls", "html", "cssls", "bashls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.ts_ls.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.html.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
 
-            lspconfig.cssls.setup({
-                capabilities = capabilities,
-            })
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
